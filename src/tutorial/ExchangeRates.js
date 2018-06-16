a9f8ca4da58484283531dbe16ed986313874f40e
Wolfgang Therrien <mtherrien@hubspot.com>
2018-06-16 17:10:06 -0400

    Eslint.

--- src/tutorial/ExchangeRates.js
+++ src/tutorial/ExchangeRates.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { Query } from "react-apollo";
-import gql from "graphql-tag";
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 
 export const ExchangeRates = () => (
   <Query
